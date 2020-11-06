3d78aa2ce38784f28c3d9a655666e2ab2a5e03e6
thientran1199 <thientran1111999@gmail.com>
2020-11-06 08:59:44 +0700

    commit rerofit

---
+++ app/src/main/java/com/example/rerofit/APIInterface.java
@@ -0,0 +1,32 @@
+package com.example.rerofit;
+
+import com.example.rerofit.pojo.MultipleResource;
+import com.example.rerofit.pojo.User;
+import com.example.rerofit.pojo.UserList;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface APIInterface {
+    @GET("/api/unknown")
+    Call<MultipleResource> doGetListResources();
+
+    @POST("/api/users")
+    Call<User> createUser(@Body User user);
+
+    @GET("/api/users?")
+    Call<UserList> doGetUserList(@Query("page") String page);
+
+    @FormUrlEncoded
+    @POST("/api/users?")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
+}
+
+
+
+
